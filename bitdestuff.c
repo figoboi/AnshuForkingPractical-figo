d97232cb37264f379f6ba3e0bfd626802f7cb3ff
anshu-dev-clg <2302184@dhempecollege.edu.in>
2025-08-20 11:08:34 +0530

    Add files via upload

---
+++ bitdestuff.c
@@ -0,0 +1,59 @@
+//bit destuffing
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char bit[50];
+    char destuff[50];
+    int i=0;
+    int j=0;
+    int count=0;
+
+    printf("Enter the sequence to be destuffed : ");
+    scanf("%s",bit);
+
+    destuff[j++]='0';
+    destuff[j++]='1';
+    destuff[j++]='1';
+    destuff[j++]='1';
+    destuff[j++]='1';
+    destuff[j++]='1';
+    destuff[j++]='1';
+    destuff[j++]='0';
+
+    for(i=0;i<strlen(bit);i++)
+    {
+        if(bit[i]=='0')
+        {
+            count=0;
+        }
+        else if(bit[i]=='1')
+        {
+            count++;
+        }
+    destuff[j]=bit[i];
+    j++;
+
+    if(count== 5)
+    {
+        count=0;
+        i++;
+    }
+    }
+    destuff[j++]='0';
+    destuff[j++]='1';
+    destuff[j++]='1';
+    destuff[j++]='1';
+    destuff[j++]='1';
+    destuff[j++]='1';
+    destuff[j++]='1';
+    destuff[j++]='0';
+
+    destuff[j]='\0';
+
+    printf("The sequence after destuffing is: ");
+    printf("%s",destuff);
+
+    return 0;
+}
